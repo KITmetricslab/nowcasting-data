b372a5b859d3e13c88802c233d926101209ce438
dwolffram <daniel-wolffram@gmx.de>
2021-11-02 13:46:22 +0100

    only download one file for testing

--- get_snapshots.R
+++ get_snapshots.R
@@ -22,36 +22,33 @@ dat_rsv_age <- get_weekly_timeseries(disease = "RSV-Infection", years = years,
 write.csv(dat_rsv_age, file = paste0("RSV_Infection/RSV_Infection-age-", today, ".csv"), row.names = FALSE)
 
 
-dat_rsv_states <- get_weekly_timeseries(disease = "RSV-Infection", years = years, 
-                                        region_level = "State")
-write.csv(dat_rsv_states, file = paste0("RSV_Infection/RSV_Infection-states-", today, ".csv"), row.names = FALSE)
-
-
-########## Seasonal influenza
-
-dir.create("Seasonal_Influenza")
-
-dat_influenza_age <- get_weekly_timeseries(disease = "Influenza, saisonal", years = years, 
-                                           region_level = "Age stratification: 1 year intervals")
-write.csv(dat_influenza_age, file = paste0("Seasonal_Influenza/Seasonal_Influenza-age-", today, ".csv"), row.names = FALSE)
-
-dat_influenza_states <- get_weekly_timeseries(disease = "Influenza, saisonal", year = "2020",
-                                              region_level = "State")
-write.csv(dat_influenza_states, file = paste0("Seasonal_Influenza/Seasonal_Influenza-states-", today, ".csv"), row.names = FALSE)
-
-
-########## Pneumococcal disease
-
-dir.create("Pneumococcal_Disease")
-
-
-dat_pneumococcal_age <- get_weekly_timeseries(disease = "Pneumococcal invasive disease", years = years, 
-                                              region_level = "Age stratification: 1 year intervals")
-write.csv(dat_pneumococcal_age, file = paste0("Pneumococcal_Disease/Pneumococcal_Disease-age-", today, ".csv"), row.names = FALSE)
-
-dat_pneumococcal_states <- get_weekly_timeseries(disease = "Pneumococcal invasive disease", years = "2020",
-                                                 region_level = "State")
-write.csv(dat_pneumococcal_states, file = paste0("Pneumococcal_Disease/Pneumococcal_Disease-states-", today, ".csv"), row.names = FALSE)
-
-
-
+# dat_rsv_states <- get_weekly_timeseries(disease = "RSV-Infection", years = years, 
+#                                         region_level = "State")
+# write.csv(dat_rsv_states, file = paste0("RSV_Infection/RSV_Infection-states-", today, ".csv"), row.names = FALSE)
+# 
+# 
+# ########## Seasonal influenza
+# 
+# dir.create("Seasonal_Influenza")
+# 
+# dat_influenza_age <- get_weekly_timeseries(disease = "Influenza, saisonal", years = years, 
+#                                            region_level = "Age stratification: 1 year intervals")
+# write.csv(dat_influenza_age, file = paste0("Seasonal_Influenza/Seasonal_Influenza-age-", today, ".csv"), row.names = FALSE)
+# 
+# dat_influenza_states <- get_weekly_timeseries(disease = "Influenza, saisonal", year = "2020",
+#                                               region_level = "State")
+# write.csv(dat_influenza_states, file = paste0("Seasonal_Influenza/Seasonal_Influenza-states-", today, ".csv"), row.names = FALSE)
+# 
+# 
+# ########## Pneumococcal disease
+# 
+# dir.create("Pneumococcal_Disease")
+# 
+# 
+# dat_pneumococcal_age <- get_weekly_timeseries(disease = "Pneumococcal invasive disease", years = years, 
+#                                               region_level = "Age stratification: 1 year intervals")
+# write.csv(dat_pneumococcal_age, file = paste0("Pneumococcal_Disease/Pneumococcal_Disease-age-", today, ".csv"), row.names = FALSE)
+# 
+# dat_pneumococcal_states <- get_weekly_timeseries(disease = "Pneumococcal invasive disease", years = "2020",
+#                                                  region_level = "State")
+# write.csv(dat_pneumococcal_states, file = paste0("Pneumococcal_Disease/Pneumococcal_Disease-states-", today, ".csv"), row.names = FALSE)
